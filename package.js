b6bf4ab45bacd9729f1a862e8f5e409b5444f8c8
Devian <info@devian.tk>
2016-03-10 02:07:10 +0100

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'devian:navigation',
-    version: '0.0.2',
+    version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'Helps creating navigation-bars or menu-bars based on FlowRouter routes and groups',
     // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.3-beta.11');
+    api.versionsFrom('1.3-beta.16');
     api.use(['ecmascript', 'check', 'mongo', 'templating', 'reactive-var']);
     api.use(['kadira:flow-router@2.10.1'])
     api.addFiles(['eachMenu.html', 'eachMenu.js'], 'client')
