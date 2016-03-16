d4f3f4dfd00b2e498785fd2fe3d6c9433e015446
Devian <info@devian.tk>
2016-03-16 12:32:00 +0100

    remove comments

--- package.js
+++ package.js
@@ -5,8 +5,6 @@ Package.describe({
     summary: 'Helps creating navigation or menu bars based on FlowRouter routes and groups',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/Dev1an/Navigation.git',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
 });
 
